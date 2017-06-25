7ed166266d1ffa9bc67f74bdc25dc5f5418d5587
Alex Nichol <unixpickle@gmail.com>
2017-06-24 20:46:20 -0400

    test epsilon greedy

---
+++ policy_test.go
@@ -0,0 +1,51 @@
+package treeagent
+
+import (
+	"math/rand"
+	"testing"
+
+	"github.com/unixpickle/approb"
+	"github.com/unixpickle/weakai/idtrees"
+)
+
+func TestEpsilonGreedy(t *testing.T) {
+	dist := testClassifier{
+		0: 0.3,
+		1: 0.5,
+		2: 0.15,
+		3: 0.05,
+	}
+	policy := &Policy{
+		Classifier: dist,
+		NumActions: 4,
+		Epsilon:    0.3,
+	}
+	sampleClassifier := func(c Classifier) int {
+		out := c.Classify(nil)
+		off := rand.Float64()
+		for i := 0; i < 4; i++ {
+			off -= out[i]
+			if off <= 0 {
+				return i
+			}
+		}
+		return 3
+	}
+	corr := approb.Correlation(50000, 0.3, func() float64 {
+		if rand.Float64() < 0.3 {
+			return float64(rand.Intn(4))
+		}
+		return float64(sampleClassifier(dist))
+	}, func() float64 {
+		return float64(sampleClassifier(policy))
+	})
+	if corr < 0.9999 {
+		t.Error("correlation should be near 1, but got", corr)
+	}
+}
+
+type testClassifier map[idtrees.Class]float64
+
+func (t testClassifier) Classify(s idtrees.AttrMap) map[idtrees.Class]float64 {
+	return t
+}
