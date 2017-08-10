aef0593a91adbb74eeb97abfb2900b657c43c443
Alex Nichol <unixpickle@gmail.com>
2017-08-10 13:31:55 -0400

    added cartpole experiment
    Right now, this experiment is worse than the greedy cloning one. The policy gets up to an average of ~65 timesteps per episode, which is far from being "solved".

---
+++ experiments/cartpole/main.go
@@ -0,0 +1,74 @@
+// Trains a decision tree on CartPole-v0 in OpenAI Gym.
+
+package main
+
+import (
+	"log"
+
+	"github.com/unixpickle/anyrl"
+	"github.com/unixpickle/anyvec/anyvec32"
+	gym "github.com/unixpickle/gym-socket-api/binding-go"
+	"github.com/unixpickle/treeagent"
+)
+
+const (
+	Host             = "localhost:5001"
+	RolloutsPerBatch = 100
+	NumBatches       = 100
+
+	Depth = 3
+)
+
+func main() {
+	// Used to create vectors.
+	creator := anyvec32.CurrentCreator()
+
+	// Connect to gym server.
+	client, err := gym.Make(Host, "CartPole-v0")
+	must(err)
+	defer client.Close()
+
+	// Create an anyrl.Env from our gym environment.
+	env, err := anyrl.GymEnv(creator, client, false)
+	must(err)
+
+	// Setup a trainer.
+	trainer := &treeagent.Trainer{
+		StepSize:   0.5,
+		EntropyReg: 0.1,
+	}
+
+	// Setup a roller with a uniformly random policy.
+	roller := &treeagent.Roller{
+		Policy:     &treeagent.Tree{Distribution: treeagent.NewActionDist(2)},
+		Creator:    creator,
+		NumActions: 2,
+	}
+
+	for batchIdx := 0; batchIdx < NumBatches; batchIdx++ {
+		// Gather episode rollouts.
+		var rollouts []*anyrl.RolloutSet
+		for i := 0; i < RolloutsPerBatch; i++ {
+			rollout, err := roller.Rollout(env)
+			must(err)
+			rollouts = append(rollouts, rollout)
+		}
+
+		// Join the rollouts into one set.
+		r := anyrl.PackRolloutSets(rollouts)
+
+		// Print the rewards.
+		log.Printf("batch %d: mean_reward=%f", batchIdx, r.Rewards.Mean())
+
+		// Train on the rollouts.
+		samples := treeagent.RolloutSamples(r)
+		targets := trainer.Targets(r, samples)
+		roller.Policy = treeagent.BuildTree(treeagent.AllSamples(targets), 4, Depth)
+	}
+}
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
