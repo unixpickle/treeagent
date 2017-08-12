8e1ab223d3eb5aeef94da112e0c88f8398bf4451
Alex Nichol <unixpickle@gmail.com>
2017-08-12 18:51:53 -0400

    more general muniverse experiment
    This will allow me to run a few different environments without having to modify the code at all. Here are some environments I'd like to try:
    
     * MinimalDots-v0
     * PenguinSkip-v0
     * MeatBoyClicker-v0

--- experiments/muniverse/env.go
+++ experiments/muniverse/env.go
@@ -1,5 +1,3 @@
-// Taken from https://github.com/unixpickle/rl-agents/blob/7af9e208f3b5aa1d83abb266e7cd2ccf64b11ac2/knightower_tree/preprocess.go.
-
 package main
 
 import (
@@ -11,21 +9,31 @@ import (
 	"github.com/unixpickle/muniverse/chrome"
 )
 
-const (
-	FrameWidth  = 320
-	FrameHeight = 480
+// NumFeatures returns the number of observation features
+// for the environment (after downsampling).
+func NumFeatures(e *muniverse.EnvSpec) int {
+	width := e.Width / 4
+	height := e.Height / 4
+	if e.Width%4 != 0 {
+		width++
+	}
+	if e.Height%4 != 0 {
+		height++
+	}
+	return width * height
+}
 
-	MaxTimestep = 480
-	TimePerStep = time.Second / 8
-
-	NumFeatures = (FrameWidth / 4) * (FrameHeight / 4)
-)
-
+// Env is an anyrl.Env wrapper around a muniverse.Env.
+//
+// Action vectors are one-hot vectors indicating which key
+// to press at each timestep.
+// No key holding is performed.
 type Env struct {
-	Env     muniverse.Env
-	Creator anyvec.Creator
+	Env         muniverse.Env
+	Creator     anyvec.Creator
+	TimePerStep time.Duration
 
-	Timestep int
+	timestep int
 }
 
 func (e *Env) Reset() (observation anyvec.Vector, err error) {
@@ -42,7 +50,7 @@ func (e *Env) Reset() (observation anyvec.Vector, err error) {
 		return
 	}
 	observation = e.simplifyImage(buffer)
-	e.Timestep = 0
+	e.timestep = 0
 	return
 }
 
@@ -50,16 +58,17 @@ func (e *Env) Step(action anyvec.Vector) (observation anyvec.Vector,
 	reward float64, done bool, err error) {
 	var events []interface{}
 	actionIdx := anyvec.MaxIndex(action)
-	key := []string{"ArrowLeft", "ArrowRight", ""}[actionIdx]
-	if key != "" {
-		evt := chrome.KeyEvents[key]
+	actions := append([]string{""}, e.Env.Spec().KeyWhitelist...)
+	actionKey := actions[actionIdx]
+	if actionKey != "" {
+		evt := chrome.KeyEvents[actionKey]
 		evt1 := evt
 		evt.Type = chrome.KeyDown
 		evt1.Type = chrome.KeyUp
 		events = append(events, &evt, &evt1)
 	}
 
-	reward, done, err = e.Env.Step(TimePerStep, events...)
+	reward, done, err = e.Env.Step(e.TimePerStep, events...)
 	if err != nil {
 		return
 	}
@@ -73,18 +82,18 @@ func (e *Env) Step(action anyvec.Vector) (observation anyvec.Vector,
 	}
 	observation = e.simplifyImage(buffer)
 
-	e.Timestep++
-	if e.Timestep > MaxTimestep {
+	if time.Duration(e.timestep)*e.TimePerStep >= time.Minute {
 		done = true
 	}
 	return
 }
 
 func (e *Env) simplifyImage(in []uint8) anyvec.Vector {
-	data := make([]float64, 0, NumFeatures)
-	for y := 0; y < FrameHeight; y += 4 {
-		for x := 0; x < FrameWidth; x += 4 {
-			sourceIdx := (y*FrameWidth + x) * 3
+	spec := e.Env.Spec()
+	data := make([]float64, 0, NumFeatures(spec))
+	for y := 0; y < spec.Height; y += 4 {
+		for x := 0; x < spec.Width; x += 4 {
+			sourceIdx := (y*spec.Width + x) * 3
 			var value float64
 			for d := 0; d < 3; d++ {
 				value += float64(in[sourceIdx+d])
--- experiments/muniverse/main.go
+++ experiments/muniverse/main.go
@@ -1,47 +1,72 @@
-// Adapted from https://github.com/unixpickle/rl-agents/blob/7af9e208f3b5aa1d83abb266e7cd2ccf64b11ac2/knightower_tree/main.go
-
 package main
 
 import (
 	"compress/flate"
 	"encoding/json"
+	"flag"
 	"io/ioutil"
 	"log"
 	"math"
+	"os"
+	"runtime"
 	"sync"
+	"time"
 
 	"github.com/unixpickle/anydiff/anyseq"
 	"github.com/unixpickle/anyrl"
 	"github.com/unixpickle/anyvec/anyvec32"
+	"github.com/unixpickle/essentials"
 	"github.com/unixpickle/lazyseq"
 	"github.com/unixpickle/muniverse"
 	"github.com/unixpickle/rip"
 	"github.com/unixpickle/treeagent"
 )
 
-const (
-	ParallelEnvs = 4
-	BatchSize    = 128
-	LogInterval  = 16
-	Depth        = 3
-)
-
-const (
-	SaveFile = "policy.json"
-)
+type Flags struct {
+	BatchSize    int
+	ParallelEnvs int
+	LogInterval  int
+	Depth        int
+	StepSize     float64
+	SaveFile     string
+	Env          string
+	FrameTime    time.Duration
+}
 
 func main() {
+	flags := &Flags{}
+	flag.IntVar(&flags.BatchSize, "batch", 128, "rollout batch size")
+	flag.IntVar(&flags.ParallelEnvs, "numparallel", runtime.GOMAXPROCS(0),
+		"parallel environments")
+	flag.IntVar(&flags.LogInterval, "logint", 16, "episodes per log")
+	flag.IntVar(&flags.Depth, "depth", 3, "tree depth")
+	flag.Float64Var(&flags.StepSize, "step", 0.8, "step size")
+	flag.StringVar(&flags.SaveFile, "out", "policy.json", "file for saved policy")
+	flag.StringVar(&flags.Env, "env", "", "environment (e.g. Knightower-v0)")
+	flag.DurationVar(&flags.FrameTime, "frametime", time.Second/8, "time per frame")
+	flag.Parse()
+
+	if flags.Env == "" {
+		essentials.Die("Missing -env flag. See -help.")
+	}
+	spec := muniverse.SpecForName(flags.Env)
+	if spec == nil {
+		essentials.Die("unknown environment:", flags.Env)
+	}
+
+	log.Println("Run with arguments:", os.Args[1:])
+
 	// Setup vector creator.
 	creator := anyvec32.CurrentCreator()
 
 	// Create a decision tree policy.
-	policy := loadOrCreatePolicy()
+	policy := loadOrCreatePolicy(flags.SaveFile)
 
 	// Setup a Roller for producing rollouts.
 	roller := &treeagent.Roller{
 		Policy:     policy,
 		Creator:    creator,
-		NumActions: 2,
+		NumActions: 1 + len(spec.KeyWhitelist),
 
 		// Compress the input frames as we store them.
 		// If we used a ReferenceTape for the input, the
@@ -53,7 +78,7 @@ func main() {
 
 	// Setup a trainer for producing new policies.
 	trainer := &treeagent.Trainer{
-		StepSize:     0.8,
+		StepSize:     flags.StepSize,
 		TrainingMode: treeagent.LinearUpdate,
 	}
 
@@ -65,7 +90,7 @@ func main() {
 			log.Println("Gathering batch of experience...")
 
 			// Join the rollouts into one set.
-			rollouts := gatherRollouts(roller)
+			rollouts := gatherRollouts(flags, roller)
 			r := anyrl.PackRolloutSets(rollouts)
 
 			// Print the stats for the batch.
@@ -74,16 +99,18 @@ func main() {
 
 			// Train on the rollouts.
 			log.Println("Training on batch...")
-			samples := treeagent.Uint8Samples(NumFeatures, treeagent.RolloutSamples(r))
+			numFeatures := NumFeatures(spec)
+			samples := treeagent.Uint8Samples(numFeatures, treeagent.RolloutSamples(r))
 			targets := trainer.Targets(r, samples)
-			policy = treeagent.BuildTree(treeagent.AllSamples(targets), NumFeatures, Depth)
+			policy = treeagent.BuildTree(treeagent.AllSamples(targets), numFeatures,
+				flags.Depth)
 			roller.Policy = policy
 
 			// Save the new policy.
 			trainLock.Lock()
 			data, err := json.Marshal(policy)
 			must(err)
-			must(ioutil.WriteFile(SaveFile, data, 0755))
+			must(ioutil.WriteFile(flags.SaveFile, data, 0755))
 			trainLock.Unlock()
 		}
 	}()
@@ -96,17 +123,17 @@ func main() {
 	trainLock.Lock()
 }
 
-func gatherRollouts(roller *treeagent.Roller) []*anyrl.RolloutSet {
-	resChan := make(chan *anyrl.RolloutSet, BatchSize)
+func gatherRollouts(flags *Flags, roller *treeagent.Roller) []*anyrl.RolloutSet {
+	resChan := make(chan *anyrl.RolloutSet, flags.BatchSize)
 
-	requests := make(chan struct{}, BatchSize)
-	for i := 0; i < BatchSize; i++ {
+	requests := make(chan struct{}, flags.BatchSize)
+	for i := 0; i < flags.BatchSize; i++ {
 		requests <- struct{}{}
 	}
 	close(requests)
 
 	var wg sync.WaitGroup
-	for i := 0; i < ParallelEnvs; i++ {
+	for i := 0; i < flags.ParallelEnvs; i++ {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
@@ -123,8 +150,9 @@ func gatherRollouts(roller *treeagent.Roller) []*anyrl.RolloutSet {
 			defer env.Close()
 
 			preproc := &Env{
-				Env:     env,
-				Creator: roller.Creator,
+				Env:         env,
+				Creator:     roller.Creator,
+				TimePerStep: flags.FrameTime,
 			}
 			for _ = range requests {
 				rollout, err := roller.Rollout(preproc)
@@ -146,7 +174,7 @@ func gatherRollouts(roller *treeagent.Roller) []*anyrl.RolloutSet {
 		res = append(res, item)
 		numBatchReward++
 		batchRewardSum += item.Rewards.Mean()
-		if numBatchReward == LogInterval || len(res) == BatchSize {
+		if numBatchReward == flags.LogInterval || len(res) == flags.BatchSize {
 			log.Printf("sub_mean=%f", batchRewardSum/float64(numBatchReward))
 			numBatchReward = 0
 			batchRewardSum = 0
@@ -155,8 +183,8 @@ func gatherRollouts(roller *treeagent.Roller) []*anyrl.RolloutSet {
 	return res
 }
 
-func loadOrCreatePolicy() *treeagent.Tree {
-	data, err := ioutil.ReadFile(SaveFile)
+func loadOrCreatePolicy(path string) *treeagent.Tree {
+	data, err := ioutil.ReadFile(path)
 	if err != nil {
 		log.Println("Created new policy.")
 		return &treeagent.Tree{Distribution: treeagent.NewActionDist(2)}
